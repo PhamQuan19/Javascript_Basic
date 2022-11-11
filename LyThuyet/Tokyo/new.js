c924d9a8cd765d54e2a4acfe7ce4d2085412b1ab
PhamQuan19 <89699511+PhamQuan19@users.noreply.github.com>
2022-11-11 10:06:47 +0700

    Update new.js

--- LyThuyet/Tokyo/new.js
+++ LyThuyet/Tokyo/new.js
@@ -21,4 +21,33 @@ var mouse2=new Mouse('black',0.2);
 
 console.log(mouse1);
 console.log(mouse2);
+//==================BT=======================
+var tom={
+    name:'Tom',
+    stomach:[],
+    eat: function(mouse){
+        this.stomach.push(mouse);
+        return this;
+    }
+};
 
+function Mouse1(name){
+    this.name=name;
+}
+
+var m1=new Mouse1('m1');
+var m2=new Mouse1('m2');
+var m3=new Mouse1('m3');
+
+tom.eat(m1).eat(m2).eat(m3);
+console.log(tom);
+
+console.log('===============');
+
+var m1={name:'m1'};
+var m2={name:'m2'};
+var m3={name:'m3'};
+
+tom.eat(m1).eat(m2).eat(m3); //method chaining
+console.log('Tom:');
+console.log(tom);
